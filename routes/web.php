30737317d1c802333644bdeee0ab865b79149033
Luis F. Franco, PhD, MOU <lffrancoj@fertinal.com>
2026-05-20 18:08:37 -0600

    Corrección de página principal

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
-Route::livewire('/', 'pages::welcome');
+Route::livewire('/', 'pages::auth.login')->name('home');
 
 Route::livewire('/login', 'pages::auth.login')->name('login');
 Route::livewire('/recover', 'pages::auth.recover')->name('recover');
