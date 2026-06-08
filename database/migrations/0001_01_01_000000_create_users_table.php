848139013815fd1b20cc7d71eae5ff4a2585eb32
Luis F. Franco, PhD, MOU <lffrancoj@fertinal.com>
2026-06-08 12:12:43 -0600

    Adecuación de tabla de usuarios y configuración de roles y permisos

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -6,44 +6,61 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
-        });
+  /**
+   * Run the migrations.
+   */
+  public function up(): void
+  {
+    Schema::create('users', function (Blueprint $table) {
+      $table->id();
+      $table->string('nombre');
+      $table->string('email')->unique();
 
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
-        });
+      // Para los empleados de Fertinal, número de empleado
+      $table->string('empleado')->nullable()->unique();
+      $table->string('rfc')->nullable()->unique();
+      $table->enum('estado', ['inactivo', 'pendiente', 'activo', 'bloqueado'])
+        ->default('pendiente')
+        ->index();
 
-        Schema::create('sessions', function (Blueprint $table) {
-            $table->string('id')->primary();
-            $table->foreignId('user_id')->nullable()->index();
-            $table->string('ip_address', 45)->nullable();
-            $table->text('user_agent')->nullable();
-            $table->longText('payload');
-            $table->integer('last_activity')->index();
-        });
-    }
+      // Verificación de correo electrónico
+      $table->timestamp('email_verified_at')->nullable();
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('users');
-        Schema::dropIfExists('password_reset_tokens');
-        Schema::dropIfExists('sessions');
-    }
+      $table->string('password')->nullable();
+
+      $table->string('avatar')->nullable();
+      $table->string('id_externo')->nullable();
+      $table->string('auth_externo')->default('google');
+
+      $table->rememberToken();
+
+      $table->softDeletes();
+      $table->timestamps();
+    });
+
+    Schema::create('password_reset_tokens', function (Blueprint $table) {
+      $table->string('email')->primary();
+      $table->string('token');
+      $table->timestamp('created_at')->nullable();
+    });
+
+    Schema::create('sessions', function (Blueprint $table) {
+      $table->string('id')->primary();
+      $table->foreignId('user_id')->nullable()->index();
+      $table->string('ip_address', 45)->nullable();
+      $table->text('user_agent')->nullable();
+      $table->longText('payload');
+      $table->integer('last_activity')->index();
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   */
+  public function down(): void
+  {
+    Schema::dropIfExists('users');
+    Schema::dropIfExists('password_reset_tokens');
+    Schema::dropIfExists('sessions');
+  }
 };
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,21 +5,41 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
+  use WithoutModelEvents;
 
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
-    {
-        // User::factory(10)->create();
+  /**
+   * Seed the application"s database.
+   */
+  public function run(): void
+  {
+    $user = User::factory()->create([
+      // nombre
+      // email
+      // empleado
+      // rfc
+      // estado
+      // email_verified_at
+      // password
+      // avatar
+      // id_externo
+      // auth_externo
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
-    }
+      "nombre"    => "Luis Fernando Franco",
+      "email"     => "lffrancoj@fertinal.com",
+      "empleado"  => "10000341",
+      "rfc"       => "FAJL710523QP0",
+      "estado"    => "activo",
+    ]);
+
+    $r = Role::create(['name' => 'admin']);
+    $p = Permission::create(['name' => 'gestionar usuarios']);
+    $r->givePermissionTo($p);
+    $user->assignRole('admin');
+
+  }
 }
