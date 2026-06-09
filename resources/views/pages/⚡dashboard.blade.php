97d06dadf0940efba2922bdb4017f9c0d28bd58c
Luis F. Franco, PhD, MOU <lffrancoj@fertinal.com>
2026-06-09 12:58:37 -0600

    Fix: Adecuación de dashboard

--- resources/views/pages/⚡dashboard.blade.php
+++ resources/views/pages/⚡dashboard.blade.php
@@ -20,20 +20,22 @@ new class extends Component
 };
 ?>
 
-<div class="space-y-4">
+<x-card class="mt-12 relative">
   @if ($user->avatar)
-    <div class="flex items-center justify-center">
+    <div class="absolute -top-12 left-1/2 transform -translate-x-1/2">
       <img src="{{ $user->avatar }}" alt="Avatar" class="w-24 h-24 rounded-full">
     </div>
   @endif
-  @if ($user->external_id)
-    <p class="text-xl font-bold text-center">{{ $user->external_id }}</p>
+  @if ($user->id_externo)
+    <div class="absolute text-base-content/50 top-12 left-1/2 transform -translate-x-1/2 text-xl font-bold">{{ $user->id_externo }}</div>
   @endif
-  <p>Aquí podrás ver un resumen de tus actividades, estadísticas y más.</p>
-  <p>¡Explora las diferentes secciones para aprovechar al máximo Blat!</p>
-  <x-button
-    wire:click='logout'
-    class="btn-error"
-    label="Logout"
-    />
-</div>
+  <div class="mt-18 space-y-4">
+    <div>Aquí podrás ver un resumen de tus actividades, estadísticas y más.</div>
+    <div>¡Explora las diferentes secciones para aprovechar al máximo Blat!</div>
+    <x-button
+      wire:click='logout'
+      class="btn-error"
+      label="Logout"
+      />
+  </div>
+</x-card>
